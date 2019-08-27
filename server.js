ac9cb9556fba517440c533d41564e60fd3b1a394
Jason Etcovitch <jasonetco@github.com>
2019-08-27 16:57:15 -0400

    Split BANNED_CHANNELS out of the middleware

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ const KeepAlive = require('./keep-alive')
 // Tiny logger to prevent logs in tests
 const log = process.env.NODE_ENV === 'test' ? _ => _ : console.log
 
+// Get an array of banned channel names
+const bannedChannels = process.env.BANNED_CHANNELS && process.env.BANNED_CHANNELS.split(',')
+
 // Middleware to bail early if the channel is banned
 function channelIsBanned (req, res, next) {
   // Can't use the req.param here because the route hasn't been defined
   const channel = req.originalUrl.slice(1)
-  if (channel) {
-    const bannedChannels = process.env.BANNED_CHANNELS && process.env.BANNED_CHANNELS.split(',')
-    if (bannedChannels && bannedChannels.includes(channel)) {
+  if (channel && bannedChannels && bannedChannels.includes(channel)) {
       return res.status(403).send('Channel has been disabled due to too many connections.')
     }
-  }
 
   next()
 }
