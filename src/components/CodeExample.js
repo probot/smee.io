f79f8e43c96dd7d402c6c8c25f4cc7186fbe743e
Jason Etcovitch <jasonetco@github.com>
2019-09-01 23:18:17 -0400

    Convert CodeExample to not use dangerouslySetHTML

--- src/components/CodeExample.js
+++ src/components/CodeExample.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
-const code = `<span class="hljs-keyword">const</span> SmeeClient = <span class="hljs-built_in">require</span>(<span class="hljs-string">'smee-client'</span>)
-
-<span class="hljs-keyword">const</span> smee = <span class="hljs-keyword">new</span> SmeeClient({
-<span class="hljs-attr">  source</span>: <span class="hljs-string">'${window.location.href}'</span>,
-<span class="hljs-attr">  target</span>: <span class="hljs-string">'http://localhost:3000/events'</span>,
-<span class="hljs-attr">  logger</span>: <span class="hljs-built_in">console</span>
-})
-
-<span class="hljs-keyword">const</span> events = smee.start()
-
-<span class="hljs-comment">// Stop forwarding events</span>
-events.close()`
-
-export default function CodeExample () {
+/**
+ * This is a weird component that is the output of HighlightJS, but as
+ * a hard-coded component. This is done to omit HighlightJS from the bundle,
+ * since the output is always exactly the same.
+ *
+ * React doesn't have great support for new-lines, so <br /> tags are used instead.
+ */
+export default React.memo(function CodeExample () {
   return (
-    <pre dangerouslySetInnerHTML={{ __html: code }} />
+    <pre>
+      <span className="hljs-keyword">const</span> SmeeClient = <span className="hljs-built_in">require</span>(<span className="hljs-string">'smee-client'</span>)
+      <br /><br />
+      <span className="hljs-keyword">const</span> smee = <span className="hljs-keyword">new</span> SmeeClient({'{'}<br />
+      <span className="hljs-attr">  source</span>: <span className="hljs-string">'{window.location.href}'</span>,<br />
+      <span className="hljs-attr">  target</span>: <span className="hljs-string">'http://localhost:3000/events'</span>,<br />
+      <span className="hljs-attr">  logger</span>: <span className="hljs-built_in">console</span><br />
+      {'}'})<br /><br />
+      <span className="hljs-keyword">const</span> events = smee.start()<br /><br />
+      <span className="hljs-comment">// Stop forwarding events</span><br />
+      events.close()
+    </pre>
   )
-}
+})
