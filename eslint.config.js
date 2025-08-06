3d3aee25879ae5ff2fa9dcd980b439fbc6311c33
wolfy1339 <webmaster@wolfy1339.com>
2025-08-06 19:33:19 -0400

    style: seperate test files from source files

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
-const globals = require('globals')
+const neostandard = require('neostandard')
 
-module.exports = require('neostandard')({
-  globals: { ...globals.jest },
-  ignores: [
-    'node_modules',
-    'public',
-  ]
-})
+module.exports = [
+  ...neostandard({
+    env: ['browser', 'es2022'],
+    files: ['src/**/*.js', 'src/**/*.jsx', 'index.js', 'lib/**/*.js'],
+  }),
+  ...neostandard({
+    env: ['jest'],
+    files: ['test/**/*.js', 'test/**/*.jsx'],
+    ignores: [
+      'node_modules',
+      'public',
+    ]
+  })
+]
