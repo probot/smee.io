8074eec54bf8a5c7d6a153e0b31e46b3987dd2b4
Aras Abbasi <aras.abbasi@googlemail.com>
2025-08-07 04:29:16 +0200

    chore: remove date-fns (#214)

--- src/components/EventDescription.jsx
+++ src/components/EventDescription.jsx
@@ -1,13 +1,50 @@
 import React from 'react'
 import { string, object, number } from 'prop-types'
-import { format } from 'date-fns'
+
+const DayLookup = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+]
+
+const MonthLookup = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
+
+const DateSuffixLookup = [
+  '',
+  '1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th',
+  '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th',
+  '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th',
+  '31st'
+]
+
+/** @param {Date} date */
+function formatDate (date) {
+  return `${DayLookup[date.getDay()]}, ${MonthLookup[date.getMonth()]} ${DateSuffixLookup[date.getDate()]} ${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${date.getHours() >= 12 ? 'p.m. ': 'a.m.'}`
+}
 
 export default function EventDescription ({
   event,
   payload,
   timestamp
 }) {
-  const formattedTime = format(timestamp, 'EEEE, MMMM do yyyy, k:mm:ss aaaa')
+  const formattedTime = formatDate(new Date(timestamp))
   const onARepo = payload.repository && payload.repository.full_name
   const onRepos = payload.repositories && payload.repositories.every(r => r.full_name)
 
